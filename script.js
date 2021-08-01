ffbe7fc6cdc1eb4aeffb5c70f3e3850c4849ed0a
smallwisp <4ubarov96@mail.ru>
2021-08-01 19:32:27 +0300

    changed script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let num = 266219;
 let str = num.toString();
 let res = 1;
-for (const i of arr) {
+for (const i of str) {
    res *= i
 }
 
 let numPow = (res ** 3).toString();
 
-console.log(+(numPow[0] + numPow[1]));
+console.log(+numPow.slice(0, 2));
 
 
