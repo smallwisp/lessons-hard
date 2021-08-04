9b4451da89b564d7d4ec16f00de671e3683ecf1b
smallwisp <4ubarov96@mail.ru>
2021-08-04 18:45:15 +0300

    adde trim() instead of cycles

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 'use strict'
 
 function getString(str) {
-   let pos = 0;
-   let subStr = str;
+   
    if (typeof str != "string") {
       console.log('Вы ввели не строку!');
    } else {
-      while (subStr.startsWith(' ')) {
-         subStr = str.slice(pos);    
-         pos++;
-      }
-      while (subStr.endsWith(' ')) {
-         subStr = subStr.slice(0, str.length - pos);    
-         pos++;
-      }
-      if (subStr.length > 30) {
-         subStr = subStr.slice(0, subStr.length - 1) + '...'
-      }
+      
+      str = str.trim();
+      if (str.length > 30) {
+         str = str.slice(0, str.length - 1) + '...'
+      } 
+      return str;
 
    }
-   return subStr;
 }
 
 console.log(getString('       12  346748499595858fnfnfnfgjfkfkekfmdngndgj   '));
