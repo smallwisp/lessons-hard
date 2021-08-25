c41e59944de1c14d6a4e4997d833783b9b1ab754
smallwisp <46354156+smallwisp@users.noreply.github.com>
2021-08-25 16:39:55 +0300

    completed taks of lesson 15 part 2

---
+++ script.js
@@ -0,0 +1,15 @@
+ class First {
+    hello() {
+        console.log(`Привет, я метод родителя!`);
+    }
+}
+
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log(`А я наследуемый метод!`);
+    }
+}
+
+let hi = new Second();
+hi.hello();
